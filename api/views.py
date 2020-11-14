1379f10575a0a1ea4c71b63859d39dad84a62a85
Benjamin Scalise <benjamin.scalise@ict.csbe.ch>
2020-11-14 14:44:00 +0100

    works, Teil 6-7

--- api/views.py
+++ api/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import BookSerializer, AuthorSerializer, PlaceSerializer, RentSerializer, CustomerSerializer, \
+    UserSerializer
+from .models import Book, Author, Customer, Place, Rent
+from django.contrib.auth.models import User
+from .permissions import IsAuthenticatedOrPostOnly
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Book.objects.all().order_by('title')
+    serializer_class = BookSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Author.objects.all().order_by('first_name')
+    serializer_class = AuthorSerializer
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Place.objects.all().order_by('postcode')
+    serializer_class = PlaceSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Customer.objects.all().order_by('first_name')
+    serializer_class = CustomerSerializer
+
+
+class RentViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    queryset = Rent.objects.all().order_by('-begin')
+    serializer_class = RentSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrPostOnly]
+
+    queryset = User.objects.all().order_by('username')
+    serializer_class = UserSerializer
